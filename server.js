d6e62d1bade2d8176c3d9d5df7c671025c629eae
di99gq <di99gq@gmail.com>
2026-08-22 22:45:48 +0500

    обновил сервер

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: '50mb' }));
 // CORS
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); // ← ДОБАВЛЕН PUT
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     if (req.method === 'OPTIONS') return res.sendStatus(200);
     next();
